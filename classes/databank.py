993f8e323a905756f6e67cd8ac822522efc0fc03
vidageri <vgere02@hotmail.com>
2023-06-02 11:16:46 +0200

    Revert "modified"
    This reverts commit b3b591c04f408cf2f0b3a8fc188e1a9403a03c17.

--- classes/databank.py
+++ classes/databank.py
@@ -1,6 +1,5 @@
 import json
 import os
-from pathlib import Path
 
 class DataBank():
     data = False
@@ -11,7 +10,6 @@ class DataBank():
 
     def get(self, key, default=False):
         return self.data[key] if (self.data and key and self.data[key]) else default
-
 
 def save(file, data, tojson=False):
     try:
@@ -26,21 +24,20 @@ def save(file, data, tojson=False):
 
     except Exception as err:
         print(f'Exception: {err}')
+    
     return False
-
 
 def load(file, isjson=False):
     try:
-        project_path = str(Path(__file__).parent.parent)
-        if not os.path.exists(project_path + "/databank"):
-            os.makedirs(project_path + "/databank")
+        if not os.path.exists("./databank"):
+            os.makedirs("./databank")
 
-        if os.path.exists(project_path + "/databank"):
-            f = open(project_path + f"/databank/{file}", "r")
+        if os.path.exists("./databank"):
+            f = open(f"./databank/{file}", "r")
             data = f.read()
             f.close()
             return DataBank(data) if isjson else data
-
+        
     except Exception as err:
         print(f'Exception: {err}')
 
